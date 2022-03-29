dc00965239e4e1bbe2519e5888e5e62c1171857e
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-03-29 15:11:56 +0300

    UI edit-3 (Added second activity)

--- front-end/CurrencyConverterTeam/app/src/main/java/com/example/currencyconverter_team/MainActivity.java
+++ front-end/CurrencyConverterTeam/app/src/main/java/com/example/currencyconverter_team/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.currencyconverter_team;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button= findViewById(R.id.Button1);
+    }
+
+
+    public void onClick(View view){
+        Intent intent= new Intent(this,MainActivity2.class);
+
     }
 }
