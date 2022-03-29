89b4b1eb5b895b0c7446d8b182c3bb18c877ef04
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-03-29 15:13:46 +0300

    UI edit-4 (linked the two activities)

--- front-end/CurrencyConverterTeam/app/src/main/java/com/example/currencyconverter_team/MainActivity.java
+++ front-end/CurrencyConverterTeam/app/src/main/java/com/example/currencyconverter_team/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view){
         Intent intent= new Intent(this,MainActivity2.class);
+        startActivity(intent);
 
     }
 }
