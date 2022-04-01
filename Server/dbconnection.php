24784c68dd90b873f382531016ea50a9f422161e
Yara Chahine <79076389+YaraChahine@users.noreply.github.com>
2022-04-01 20:36:09 +0300

    Apis modifications

--- Server/dbconnection.php
+++ Server/dbconnection.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = null;
-$db_name = "mobiledb";
+$db_name = "currencyconverterdb";
 
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
--- front-end/Currency Converter/app/src/main/java/com/example/currencyconverter_team/MainActivity2.java
+++ front-end/Currency Converter/app/src/main/java/com/example/currencyconverter_team/MainActivity2.java
@@ -3,15 +3,18 @@ package com.example.currencyconverter_team;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Debug;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 
 import org.json.JSONObject;
 
@@ -27,77 +30,135 @@ public class MainActivity2 extends AppCompatActivity {
     RadioButton usd;
     EditText amount;
     TextView result;
+    String result2 = "";
 
-    public class DownloadTask extends AsyncTask<String, Void, String>{
 
-        @Override
-        protected String doInBackground(String... urls) {
+    public class DownloadTask extends AsyncTask<String, Void, String> {
 
-            String result="";
-
+        protected String doInBackground(String... urls){
             URL url;
             HttpURLConnection http;
 
-            try{
-                //Reading json Objects from API
-                url = new URL(urls[0]);
-                http = (HttpURLConnection) url.openConnection(); // connection established between our application and API
-                InputStream in = http.getInputStream();
-                InputStreamReader reader = new InputStreamReader(in); // used to read the output of the API
-                int data =  reader.read();
 
-                while(data != -1){
-                    char current = (char) data; // parse each character
-                    result += current; //add this character to array
-                    data =reader.read(); //Data is changed not to run into infinite loop (not optimal solution)
+            try{
+                url = new URL(urls[0]);
+
+                http = (HttpURLConnection) url.openConnection();
+                http.setConnectTimeout(30000);
+
+
+                InputStream in = http.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while( data != -1){
+                    char current = (char) data;
+                    result2 += current;
+                    data = reader.read();
 
                 }
-            }
-            catch(Exception e){
+
+
+            }catch(Exception e){
                 e.printStackTrace();
+
                 return null;
             }
 
-            return result;
+            return result2;
         }
 
-        @Override
-        protected void onPostExecute(String s) { // We get the string returned from the function above
-            Log.i("Result", s);
 
-            //Handle the data(fetch and convert string s to json)
-            //Use this function to post the rate on the screen from the website lirarate.com
-            try {
-                JSONObject json = new JSONObject(s);
-                String created_at = json.getString("created_at");
-                Log.i("created_at",created_at);
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            try{
+//
+
+
 
             }catch(Exception e){
-
+                e.printStackTrace();
             }
-
-            super.onPostExecute(s);
         }
+
     }
+
+
+
+
+//    public class DownloadTask2 extends AsyncTask<String, Void, String> {
+//
+//        protected String doInBackground(String... urls){
+//            URL url;
+//            HttpURLConnection http;
+//
+//
+//            try{
+//                url = new URL(urls[0]);
+//
+//                http = (HttpURLConnection) url.openConnection();
+//                http.setConnectTimeout(30000);
+//
+//
+//                InputStream in = http.getInputStream();
+//
+//                InputStreamReader reader = new InputStreamReader(in);
+//
+//                int data = reader.read();
+//
+//                while( data != -1){
+//                    char current = (char) data;
+//                    result2 += current;
+//                    data = reader.read();
+//
+//                }
+//
+//
+//            }catch(Exception e){
+//                e.printStackTrace();
+//
+//                return null;
+//            }
+//
+//            return result2;
+//        }
+//
+//
+//        protected void onPostExecute(String s){
+//            super.onPostExecute(s);
+//
+//            try{
+//
+//                //  JSONObject json = new JSONObject(s);
+//                //  String created_at = json.getString("amount_result");
+//                Log.i("currency", s);
+//
+//                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+//
+//            }catch(Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-        String amount1 = "44";//get the amount from the view
-        String url="http://localhost/Server/api1.php?amount =" + amount1;
-
-        DownloadTask task = new DownloadTask();
-        task.execute(url);
-
-
-
         Intent x = getIntent();
         radioGrp= (RadioGroup) findViewById(R.id.radioGroup);
         amount= (EditText)  findViewById(R.id.amount);
         lbp= (RadioButton) findViewById(R.id.lbp);
         usd= (RadioButton) findViewById(R.id.usd);
         result= (TextView) findViewById(R.id.result);
+
+
+
+
+
 
     }
 
@@ -113,8 +174,12 @@ public class MainActivity2 extends AppCompatActivity {
                 if(lbp.isChecked()){
                     amount_final= amount_res*22000;
                     Integer amnt= new Integer(amount_final);
+
                     String res=amnt.toString()+ " L.L";
-                    result.setText(res);
+                    String url = "http://10.21.149.208/apis/test.php?amount="+amount_res+"&currency=lbp";
+                    DownloadTask task = new DownloadTask();
+                    task.execute(url);
+
 
 
                 }
@@ -123,7 +188,10 @@ public class MainActivity2 extends AppCompatActivity {
                     amount_final= amount_res/22000;
                     Integer amnt= new Integer(amount_final);
                     String res=amnt.toString()+ " $";
-                    result.setText(res);
+                    String url = "http://10.21.149.208/apis/test.php?amount="+amount_res+"&currency=usd";
+                    DownloadTask task = new DownloadTask();
+                    task.execute(url);
+
 
                 }
 
