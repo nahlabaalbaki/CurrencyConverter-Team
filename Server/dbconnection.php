eb3117b8d66b3b5bd477c6a7e5df803c0875c762
Nahla Baalbaki <nahla_wbaalbaki@hotmail.com>
2022-04-03 16:27:39 +0300

    adding comments-5

--- Server/dbconnection.php
+++ Server/dbconnection.php
@@ -5,7 +5,7 @@ $db_user = "root";
 $db_pass = null;
 $db_name = "currencyconverterdb";
 
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name); //creating the connection with the database 
 
 if(mysqli_connect_errno()){
     die("Conenction Failed!");
